05db90c88a5fd8ea1f712e321ae961f6a659cd1f
Garvita Jain <garvita.grd.us@gmail.com>
2025-07-28 15:35:29 -0400

    Creating multiple complex HTML templates for Base, Home Page and About Page

--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+posts = [
+    {
+        'author': '1st Author',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': '2nd Author',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2020'
+    }
+]
 
 # Create your views here.
 
+def home(request):
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
+
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
